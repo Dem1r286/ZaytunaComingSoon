b135957c9782fcb1c14f889a34c8e2313ff05aa6
Dem1r286 <yourgithubemail@gmail.com>
2025-03-15 02:02:23 +0100

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,10 @@ function App() {
         rel="noopener noreferrer"
         className="group relative inline-flex items-center mt-10 justify-center p-0.5 mb-2 mr-2 text-sm font-medium text-gray-900 rounded-lg bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
       >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Instagram
-        </span>
+        <div className="flex justify-center items-center gap-4 px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          Go Back To Instagram
+          <img src="./instagram.png" alt="" className="w-10"/>
+        </div>
         <div className="hidden group-hover:block">
           <div className="group absolute -top-12 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-slate-300 before:-top-2">
             <div className="rounded-sm bg-black py-1 px-2">
